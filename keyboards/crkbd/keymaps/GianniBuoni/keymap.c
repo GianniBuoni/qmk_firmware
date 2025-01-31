1603ea9bc237a73824c442f9b424377211c2df42
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-01-30 22:14:03 -0500

    add symols to number layer; add more options to switch back to default layer

--- keyboards/crkbd/keymaps/GianniBuoni/keymap.c
+++ keyboards/crkbd/keymaps/GianniBuoni/keymap.c
@@ -27,23 +27,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
     [1] = LAYOUT_split_3x6_3(
-    XXXXXXX,  KC_PAST,  KC_HASH,  KC_PERC,  KC_DQUO,  KC_AT,    KC_LT,    KC_QUOT,  KC_GRV,   KC_GT,    KC_TRNS,  XXXXXXX,
+    XXXXXXX,  KC_PAST,  KC_HASH,  KC_PERC,  KC_DQUO,  KC_AT,    KC_LT,    KC_QUOT,  KC_GRV,   KC_BSLS,  KC_GT,  XXXXXXX,
     XXXXXXX,  KC_CIRC,  KC_PMNS,  KC_PPLS,  KC_DLR,   KC_PEQL,  KC_LPRN,  KC_LCBR,  KC_RCBR,  KC_RPRN,  TO(3),    XXXXXXX,
     XXXXXXX,  KC_TILD,  KC_UNDS,  KC_BSLS,  KC_BSLS,  KC_AMPR,  KC_LBRC,  KC_RBRC,  KC_QUES,  KC_PIPE,  KC_EXLM,  XXXXXXX,
                                   TO(0),    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  TO(2)
   ),
 
     [2] = LAYOUT_split_3x6_3(
-    XXXXXXX,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_1,    KC_2,  KC_3,  KC_TRNS,  XXXXXXX,
-    XXXXXXX,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_4,    KC_5,  KC_6,  KC_TRNS,  XXXXXXX,
-    XXXXXXX,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_0,     KC_7,    KC_8,  KC_9,  KC_DOT,   XXXXXXX,
-                                  TO(0),    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, OSL(1)
+    XXXXXXX,  KC_PAST,          KC_HASH,          KC_PERC,          KC_DQUO,          KC_AT,    KC_LT,    KC_1,    KC_2,  KC_3,  KC_GT,  XXXXXXX,
+    XXXXXXX,  LGUI_T(KC_CIRC),  LALT_T(KC_PMNS),  LCTL_T(KC_PPLS),  LSFT_T(KC_DLR),   KC_PEQL,  KC_TRNS,  KC_4,    KC_5,  KC_6,  KC_TRNS,  XXXXXXX,
+    XXXXXXX,  KC_TILD,          KC_UNDS,          KC_BSLS,          KC_BSLS,          KC_AMPR,  KC_0,     KC_7,    KC_8,  KC_9,  KC_DOT,   XXXXXXX,
+                                                  TO(0),            KC_TRNS,          KC_TRNS,  TO(0),  KC_TRNS, OSL(1)
   ),
 
     [3] = LAYOUT_split_3x6_3(
-    XXXXXXX,  KC_Q,  KC_W,  KC_E,     KC_J,     KC_ESC,   KC_DOWN,  KC_RGHT,        KC_TRNS,        KC_TRNS,         KC_TRNS,         XXXXXXX,
-    XXXXXXX,  KC_A,  KC_S,  KC_D,     KC_I,     KC_TAB,   KC_F2,    RSFT_T(KC_F4),  RCTL_T(KC_F5),  RALT_T(KC_F11),  RGUI_T(KC_F12),  XXXXXXX,
-    XXXXXXX,  KC_Z,  KC_X,  KC_C,     KC_M,     KC_LALT,  KC_UP,    KC_LEFT,        KC_TRNS,        KC_TRNS,         KC_TRNS,         XXXXXXX,
-                            TO(0),    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        OSL(1)
+    XXXXXXX,  KC_Q,  KC_W,  KC_E,     KC_J,     KC_ESC,   KC_DOWN,  KC_RGHT,           KC_NO,             KC_NO,              KC_NO,              XXXXXXX,
+    XXXXXXX,  KC_A,  KC_S,  KC_D,     KC_I,     KC_TAB,   KC_F2,    MT(KC_F4, KC_NO),  MT(KC_F5, KC_NO),  MT(KC_F11, KC_NO),  MT(KC_F12, KC_NO),  XXXXXXX,
+    XXXXXXX,  KC_Z,  KC_X,  KC_C,     KC_M,     KC_LALT,  KC_UP,    KC_LEFT,           KC_NO,             KC_NO,              KC_NO,              XXXXXXX,
+                            TO(0),    KC_TRNS,  KC_TRNS,  TO(0),  KC_TRNS,           OSL(1)
   )
 };
